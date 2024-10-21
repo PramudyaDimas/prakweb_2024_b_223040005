5b0ab668920227afebce38da29d806ef303f7d8b
Pramudya <Pramudya.223040005@mail.unpas.ac.id>
2024-10-21 14:40:14 +0700

    MVC - Flash Message

--- MVC/app/controllers/Mahasiswa.php
+++ MVC/app/controllers/Mahasiswa.php
@@ -18,10 +18,19 @@ class Mahasiswa extends Controller
         $this->view('mahasiswa/detail', $data);
         $this->view('templates/footer');
     }
+
+
+
     public function  tambah() {
         if( $this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0 ) {
+            Flasher::setFlash('berhasil', 'ditambahkan', 'success');
+            header('location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }else {
+            Flasher::setFlash('gagal', 'ditambahkan', 'danger');
             header('location: ' . BASEURL . '/mahasiswa');
             exit;
         }
     }
+
 }
--- MVC/public/index.php
+++ MVC/public/index.php
@@ -1,5 +1,74 @@
-<?php
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-lg-6">
+            <?php Flasher::flash(); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-5">
+        <div class="col-lg-6">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
+            <br><br>
+            <h3>Daftar Mahasiswa</h3>
+            <ul class="list-group">
+                <?php foreach ($data['mhs'] as $mhs) : ?>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <?= $mhs['nama']; ?>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary"
+                        style="text-decoration: none;">Detail</a>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+</div>
 
-require_once '../app/init.php';
 
-$app = new App;
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="judulModal">Tambah Data Mahasiswa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nrp">NRP</label>
+                        <input type="number" class="form-control" id="nrp" name="nrp">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+
+
+                    <div class="form-group">
+                        <label for="jurusan">Example Jurusan</label>
+                        <select class="form-control" id="jurusan" name="jurusan">
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Teknik Industri">Teknik Industri</option>
+                            <option value="Teknik Mesin">Teknik Mesin</option>
+                            <option value="Teknologi Pangan">Teknologi Pangan</option>
+                            <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+                        </select>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
+        </div>
+    </div>
