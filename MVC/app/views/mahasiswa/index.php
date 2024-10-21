b99349905cc57a33b556266a10dbe2237be8a773
Pramudya <Pramudya.223040005@mail.unpas.ac.id>
2024-10-21 14:49:06 +0700

    MVC - Update Data(+ ajax)

--- MVC/app/views/mahasiswa/index.php
+++ MVC/app/views/mahasiswa/index.php
@@ -1,13 +1,17 @@
 <div class="container mt-3">
+
+
     <div class="row">
         <div class="col-lg-6">
             <?php Flasher::flash(); ?>
         </div>
     </div>
+
     <div class="row">
-        <div class="col-5">
         <div class="col-lg-6">
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+            <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal"
+                data-bs-target="#formModal">
                 Tambah Data Mahasiswa
             </button>
             <br><br>
@@ -19,6 +23,9 @@
                     <div>
                         <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>"
                             class="badge text-bg-primary text-decoration-none ms-1">Detail</a>
+                        <a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>"
+                            class="badge text-bg-warning text-decoration-none ms-1 tampilModalUbah"
+                            data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?= $mhs['id']; ?>">Ubah</a>
                         <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>"
                             class="badge text-bg-danger text-decoration-none ms-1"
                             onclick="return confirm('yakin?')">Hapus</a>
@@ -36,3 +43,46 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
+                <h1 class="modal-title fs-5" id="judulModal">Tambah Data Mahasiswa</h1>
+                <h1 class="modal-title fs-5" id="formModalLabel">Tambah Data Mahasiswa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <input type="hidden" name="id" id="id">
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nrp">NRP</label>
+                        <input type="number" class="form-control" id="nrp" name="nrp">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+
+
+                    <div class="form-group">
+                        <label for="jurusan">Example Jurusan</label>
+                        <select class="form-control" id="jurusan" name="jurusan">
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Teknik Industri">Teknik Industri</option>
+                            <option value="Teknik Mesin">Teknik Mesin</option>
+                            <option value="Teknologi Pangan">Teknologi Pangan</option>
+                            <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+                        </select>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
+        </div>
+    </div>
