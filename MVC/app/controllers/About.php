25b51b4433e3102d3071a7b53a09bc9192d1aab9
Pramudya <Pramudya.223040005@mail.unpas.ac.id>
2024-10-21 14:56:43 +0700

    MVC - Searching

--- MVC/app/controllers/About.php
+++ MVC/app/controllers/About.php
@@ -1,10 +1,7 @@
 <?php 
 
-class About {
-    public function index($nama = 'Pramudya', $pekerjaan = 'guru') {
-        echo "Hallo, nama saya  $nama, saya adalah seorang $pekerjaan";
 class About extends Controller {
-    public function index($nama = 'Pram', $pekerjaan = 'guru', $umur = 45) {
+    public function index($nama = 'Adi', $pekerjaan = 'guru', $umur = 45) {
 
         
         $data['nama'] = $nama;
@@ -12,18 +9,16 @@ class About extends Controller {
         $data['pekerjaan'] = $pekerjaan;
         $data['judul'] = 'About Me';
         $this->view('templates/header',  $data);
+
         $this->view('about/index', $data);
         $this->view('templates/footer');
          
     }
     public function page() {
-        echo 'About/page';
         
         $data['judul'] = 'Page';
         $this->view('templates/header', $data);
         $this->view('about/page');
         $this->view('templates/footer');
-    }
-}
     }
 }
